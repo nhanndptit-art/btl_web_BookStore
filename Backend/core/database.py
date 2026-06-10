3072547e7611198cd9498bbad65261e1d2b37f8c
Multiply <nhannd.ptit@gmail.com>
2026-06-10 22:47:27 +0700

    Transform products page from static to dynamic

--- Backend/core/database.py
+++ Backend/core/database.py
@@ -2,16 +2,16 @@ import mysql.connector
 from core.config import Config
 
 def get_db_connection():
-   return mysql.connector.connect(
-       host=Config.DB_HOST,
-       port=Config.DB_PORT,
-       user=Config.DB_USER,
-       password=Config.DB_PASSWORD,
-       database=Config.DB_NAME
+    return mysql.connector.connect(
+        host=Config.DB_HOST,
+        port=Config.DB_PORT,
+        user=Config.DB_USER,
+        password=Config.DB_PASSWORD,
+        database=Config.DB_NAME
    )
 
-def get_db()
-   db = None
+def get_db():
+    db = None
     try:
         db = get_db_connection()
         yield db
--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from api.routes import router as api_router
 app = FastAPI()
+
+
+app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
